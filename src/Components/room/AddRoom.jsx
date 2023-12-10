91da20198d83f4127ed4f78341e9c8017458a2cb
winmoh <mohamedaouinti2023@gmail.com>
2023-12-10 22:59:01 +0100

    adding the add room and get all rooms functionnalitites using axios requests

---
+++ src/Components/room/AddRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddRoom= () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default  AddRoom
---
+++ src/Components/utils/ApiFunction.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+export const api=axios.create({
+    baseURL:"http://loacalhost:8080/api"
+})
+
+export async function addRoom(room){
+    const roomDTO=new FormData();
+    roomDTO.append("roomType",room.roomType)
+    roomDTO.append("roomPrice",room.roomPrice)
+    roomDTO.append("rnumber",room.rnumber)
+    roomDTO.append("photo",room.photo)
+
+    const response=await api.post("/room/addRoom",roomDTO)
+    if(response.status===201){
+        return "room addedd succefully"
+    }
+    else{
+        return "the room wasn't added! review your solution"
+    }
+
+
+}
+
+export async function getAllRooms  (){
+    try{
+        const response=await api.get("/room/getAll")
+        return response.data;
+
+    }
+    catch(error){
+        throw new Error("a problem occured when trying to get th available rooms")
+    }
+
+}
